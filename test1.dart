c7590b065a782498ab7c522f698fd5feee407157
omaima-aboelfath <202101319@pua.edu.eg>
2024-11-06 19:35:55 +0200

    create test1 & test2 app bar

---
+++ test1.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Test1 extends StatelessWidget {
+  const Test1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text('test1'),
+      ),
+    );
+  }
+}
